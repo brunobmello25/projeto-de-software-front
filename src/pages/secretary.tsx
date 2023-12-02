5cec66478bda97da176e35d24e8492d990dc712b
Thauan Corrêa <thauancoroli@gmail.com>
2023-12-02 00:31:50 -0300

    Adding icon for CRUD

--- src/pages/secretary.tsx
+++ src/pages/secretary.tsx
@@ -1,59 +1,84 @@
-import { useState } from "react";
-import { Button, Modal, Form, Input, Table } from 'antd';
+// Secretaria.jsx
+import React, { useState } from 'react';
+import { Button, Modal, Form, Input, Table, Space, Popconfirm } from 'antd';
 
-  const Secretary = () => {
-    const [isModalVisible, setIsModalVisible] = useState(false);
-    const [dataSource, setDataSource] = useState([]);
-  
-    const showModal = () => {
-      setIsModalVisible(true);
-    };
-  
-    const handleOk = () => {
-      setIsModalVisible(false);
-    };
-  
-    const handleCancel = () => {
-      setIsModalVisible(false);
-    };
-  
-    const columns = [
-      {
-        title: 'Nome',
-        dataIndex: 'name',
-        key: 'name',
-      },
-      {
-        title: 'Email',
-        dataIndex: 'email',
-        key: 'email',
-      },
-    ];
-  
-    return (
-      <div>
-        <Button type="primary" onClick={showModal}>
-          Cadastrar Paciente
-        </Button>
-        <Modal
-          title="Cadastro de Paciente"
-          visible={isModalVisible}
-          onOk={handleOk}
-          onCancel={handleCancel}
-        >
-          <Form>
-            <Form.Item label="Nome">
-              <Input /> 
-            </Form.Item>
-            <Form.Item label="Email">
-              <Input type="email" /> 
-            </Form.Item>
-          </Form>
-        </Modal>
-  
-        <Table dataSource={dataSource} columns={columns} />
-      </div>
-    );
+const Secretaria = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [dataSource, setDataSource] = useState([]);
+  const [form] = Form.useForm();
+
+  const showModal = () => {
+    setIsModalVisible(true);
   };
 
-export default Secretary;
+  const handleOk = () => {
+    form.validateFields().then((values) => {
+      const updatedDataSource = [...dataSource, values];
+      setDataSource(updatedDataSource);
+      setIsModalVisible(false);
+      form.resetFields();
+    });
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+    form.resetFields();
+  };
+
+  const handleDelete = (record) => {
+    const updatedDataSource = dataSource.filter((item) => item !== record);
+    setDataSource(updatedDataSource);
+  };
+
+  const columns = [
+    {
+      title: 'Nome',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+    },
+    {
+      title: 'Ações',
+      key: 'actions',
+      render: (text, record) => (
+        <Space size="middle">
+          <a onClick={() => console.log('Editar', record)}>Editar</a>
+          <Popconfirm
+            title="Tem certeza de que deseja deletar?"
+            onConfirm={() => handleDelete(record)}
+            okText="Sim"
+            cancelText="Não"
+          >
+            <a style={{ color: 'red' }}>Deletar</a>
+          </Popconfirm>
+        </Space>
+      ),
+    },
+  ];
+
+  return (
+    <div>
+      <Button type="primary" onClick={showModal}>
+        Cadastrar Paciente
+      </Button>
+      <Modal title="Cadastro de Paciente" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+        <Form form={form}>
+          <Form.Item label="Nome" name="name" rules={[{ required: true, message: 'Por favor, insira o nome!' }]}>
+            <Input />
+          </Form.Item>
+          <Form.Item label="Email" name="email" rules={[{ required: true, message: 'Por favor, insira o email!' }]}>
+            <Input />
+          </Form.Item>
+        </Form>
+      </Modal>
+
+      <Table dataSource={dataSource} columns={columns} />
+    </div>
+  );
+};
+
+export default Secretaria;
