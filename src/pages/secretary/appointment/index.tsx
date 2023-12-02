1d479622e8edf31951bfd62e082803bf27eba730
Thauan Corrêa <tholiveira@id.uff.br>
2023-12-02 13:46:29 -0300

    Register Patients layout

--- src/pages/secretary/appointment/index.tsx
+++ src/pages/secretary/appointment/index.tsx
@@ -1,26 +1,29 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Modal, Form, DatePicker, Button } from 'antd';
+import Layout from "../../../components/layout/index"
 
 const Appointment = () => {
   const [form] = Form.useForm();
-  
+
 
   const handleOk = () => {
     form.validateFields().then((values) => {
       console.log('Agendar Consulta:', values);
       form.resetFields();
-     
+
     });
   };
 
   return (
-    <Modal title="Agendar Consulta"  onOk={handleOk} >
-      <Form form={form}>
-        <Form.Item label="Data da Consulta" name="dataConsulta" rules={[{ required: true, message: 'Por favor, escolha a data!' }]}>
-          <DatePicker />
-        </Form.Item>
-      </Form>
-    </Modal>
+    <Layout>
+      <Modal title="Agendar Consulta" onOk={handleOk} >
+        <Form form={form}>
+          <Form.Item label="Data da Consulta" name="dataConsulta" rules={[{ required: true, message: 'Por favor, escolha a data!' }]}>
+            <DatePicker />
+          </Form.Item>
+        </Form>
+      </Modal>
+    </Layout>
   );
 };
 
