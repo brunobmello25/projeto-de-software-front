91cf8bd574f198e7a2e4396b5bf831cea7df2beb
Bruno Mello <bruno.barros.mello@gmail.com>
2023-12-04 22:29:41 -0300

    fix: proper authentication

--- src/services/auth/auth.ts
+++ src/services/auth/auth.ts
@@ -1,25 +1,26 @@
-import api from '../utils/api';
-import { setTokenCookie, removeTokenCookie} from '../utils/cookie';
+import api from "../utils/api";
+import { setTokenCookie, removeTokenCookie } from "../utils/cookie";
 
 export const login = async (credentials: Object) => {
-    try {
-        const response = await api.post('/sessions', credentials);
-        if (response.status >= 200 && response.status <= 203  ) {
-            setTokenCookie(response.data.token);
-        } else {
-            console.warn('Resposta inesperada:', response);
-        }
-    } catch (error: any) {
-        if (error.response) {
-            throw new Error(`Erro ${error.response.status}: ${error.response.data.message}`);
-        } else if (error.request) {
-            throw new Error('Erro de rede ou o servidor não respondeu.');
-        } else {
-            throw new Error('Erro desconhecido ao configurar a requisição.');
-        }
+  try {
+    const response = await api.post("/sessions", credentials);
+
+    if (response.status >= 200 && response.status <= 203) {
+      setTokenCookie(response.data.token);
     }
+  } catch (error: any) {
+    if (error.response) {
+      throw new Error(
+        `Erro ${error.response.status}: ${error.response.data.message}`,
+      );
+    } else if (error.request) {
+      throw new Error("Erro de rede ou o servidor não respondeu.");
+    } else {
+      throw new Error("Erro desconhecido ao configurar a requisição.");
+    }
+  }
 };
 
 export const logout = () => {
-    removeTokenCookie();
+  removeTokenCookie();
 };
